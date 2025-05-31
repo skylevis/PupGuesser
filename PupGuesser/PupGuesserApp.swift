78d82a0c01de5288181049b85b28e71dbf4ebb3d
See Soon Kiat <skylavis@hotmail.com>
2025-05-31 14:06:05 +0800

    Initial commit of launch screen and app icon.

---
+++ PupGuesser/PupGuesserApp.swift
@@ -0,0 +1,17 @@
+//
+//  PupGuesserApp.swift
+//  PupGuesser
+//
+//  Created by See Soon Kiat on 31/5/25.
+//
+
+import SwiftUI
+
+@main
+struct PupGuesserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
